3ae075c977be0c527b19be7dbd102c243ef83c05
Gabriel <gszcode@gmail.com>
2023-05-30 20:09:32 -0300

    middleware de verificzacion de rol realizado

--- middlewares/verifyRole.js
+++ middlewares/verifyRole.js
@@ -1,7 +1,15 @@
 const UserAdmin = require('../models/authModel')
 
-const verifyRole = (req) => {
-  console.log(req.user)
+const verifyRole = async (req, res, next) => {
+  try {
+    const userRole = await UserAdmin.findById(req.user)
+
+    if (userRole.role !== 'admin') throw new Error('Sin Autorización')
+
+    return next()
+  } catch (error) {
+    return res.json({ error: error.message })
+  }
 }
 
 module.exports = verifyRole
